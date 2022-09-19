418f6f952bd8b28af6105f38f99c1121bfac4d08
pushpenndara12 <105039619+pushpenndara12@users.noreply.github.com>
2022-09-19 10:48:51 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import Task from "./Task";
+import List from "./List";
+
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            list:[],
+            count:1
+        }
+    }
+     
+    render(){
+        const taskdata=(data)=>{
+            
+            const newtask={
+                planning:data,
+                id:data
+            }
+            const update=[...this.state.list,newtask]
+
+            this.setState({
+                list:update,
+                count:this.state.count+1
+            }) 
+            
+        }
+        console.log(this.state.list)
+        return(
+            <div id="main">
+            
+            <Task
+            data3={taskdata} 
+            />
+            <List
+            data4={this.state.list}/>
+            </div>
+        )
+    }
+}
+export default App;
